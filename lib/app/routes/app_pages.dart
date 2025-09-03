b9b11a4bf7d222ae143c68ef7c9a9d52e43a10ce
iqbalbayuaji <iqbalbayuaji201@gmail.com>
2025-09-04 00:04:45 +0700

    [+] Add Login Register Page

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -4,6 +4,10 @@ import '../modules/homepage/bindings/homepage_binding.dart';
 import '../modules/homepage/views/homepage_view.dart';
 import '../modules/learning/bindings/learning_binding.dart';
 import '../modules/learning/views/learning_view.dart';
+import '../modules/login/bindings/login_binding.dart';
+import '../modules/login/views/login_view.dart';
+import '../modules/register/bindings/register_binding.dart';
+import '../modules/register/views/register_view.dart';
 
 part 'app_routes.dart';
 
@@ -22,6 +26,16 @@ class AppPages {
       name: _Paths.LEARNING,
       page: () => const LearningView(),
       binding: LearningBinding(),
+    ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => const LoginView(),
+      binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.REGISTER,
+      page: () => const RegisterView(),
+      binding: RegisterBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -5,10 +5,14 @@ abstract class Routes {
   Routes._();
   static const HOMEPAGE = _Paths.HOMEPAGE;
   static const LEARNING = _Paths.LEARNING;
+  static const LOGIN = _Paths.LOGIN;
+  static const REGISTER = _Paths.REGISTER;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOMEPAGE = '/homepage';
   static const LEARNING = '/learning';
+  static const LOGIN = '/login';
+  static const REGISTER = '/register';
 }
